f5c82442a462659a33a946a18821344baa2b4b6d
Oscar2991 <oalcantarac@outlook.com>
2015-11-05 16:00:54 -0600

    Actualización de consulta de Oficios Entrantes.

--- app/controllers/OficiosController.php
+++ app/controllers/OficiosController.php
@@ -5,7 +5,10 @@ class OficiosController extends BaseController {
 	public function oficialia_recibidos()
 		{
 			$oficios= OficioEntrante::join('correspondencia','Correspondencia_Id','=','Correspondencia.IdCorrespondencia')
-									->join('dependencia','Dependencia_Id','=','Dependencia.IdDependencia')
+									->join('entidad_externa','Emisor','=','Entidad_Externa.IdEntidadExterna')
+									->join('dependencia_area','entidad_externa.Dependencia_Area_Id','=','dependencia_area.IdDependenciaArea')
+									->join('dependencia_tiene_area','dependencia_area.IdDependenciaArea','=','dependencia_tiene_area.DepArea_Id')
+									->join('dependencia','dependencia_tiene_area.Dependencia_Id','=','dependencia.IdDependencia')
 									->get();
 			$estatus = Estatus::all();
 			return View::make('oficios.oficialia_recibidos',array('oficios'=>$oficios,'estatus'=>$estatus));
