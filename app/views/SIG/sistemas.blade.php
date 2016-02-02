9589e4c09bd702a43aa64805fc66a27308b9f38f
Ian Castañeda <jonatan_ian@outlook.com>
2016-02-02 14:03:33 -0600

    Editables de Sistemas Y banco de datos

--- app/views/SIG/sistemas.blade.php
+++ app/views/SIG/sistemas.blade.php
@@ -58,4 +58,39 @@
 			</table>
     </div>
 
+		<div class="content-header">
+	      <h2 class="text-primary">Editables</h2>
+				<table class="table table-striped">
+					<tr>
+						<td>PROCEDIMIENTO DE CONTROL DE DOCUMENTOS</td>
+						<td><a href = '../procedimientos/DSBD/EDITABLES/PORC_CTLDOC 290116.docx' class='btn btn-primary' target="_blank">Descargar</a></td>
+					</tr>
+
+					<tr>
+						<td>INSTRUCTIVO PARA DEFINIR LA CLAVE DE LOS DOCUMENTOS DEL SIG</td>
+						<td><a href = '../procedimientos/DSBD/EDITABLES/InstructivoNomenclaturaDocumentos.docx' class='btn btn-primary' target="_blank">Descargar</a></td>
+					</tr>
+
+					<tr>
+						<td>PLANTILLA PARA LA CREACIÓN DE PROCEDIMIENTOS</td>
+						<td><a href = '../procedimientos/DSBD/EDITABLES/PlantillaDocumentos 290116.docx' class='btn btn-primary' target="_blank">Descargar</a></td>
+					</tr>
+
+					<tr>
+						<td>PLANTILLA PARA LA CREACIÓN DE INSTRUCTIVOS</td>
+						<td><a href = '../procedimientos/DSBD/EDITABLES/Planilla Instructivo 290116.docx' class='btn btn-primary' target="_blank">Descargar</a></td>
+					</tr>
+
+					<tr>
+						<td>SOLICITUD PARA LA APROBACIÓN DE DOCUMENTOS</td>
+						<td><a href = '../procedimientos/DSBD/EDITABLES/F-JS-APR_DOC 290116.xlsx' class='btn btn-primary' target="_blank">Descargar</a></td>
+					</tr>
+
+					<tr>
+						<td>LISTA MAESTRA</td>
+						<td><a href = '../procedimientos/DSBD/EDITABLES/LISTA MAESTRA 290116.xlsx' class='btn btn-primary' target="_blank">Descargar</a></td>
+					</tr>
+				</table>
+	    </div>
+
 @stop
