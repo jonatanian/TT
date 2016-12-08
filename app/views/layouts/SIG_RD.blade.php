5ee1f2f8f0b87f3be4cf9343ef2238bd54af825f
Ian Castañeda <jonatan_ian@outlook.com>
2016-12-08 10:11:14 -0600

    modificación del menú de configuración

--- app/views/layouts/SIG_RD.blade.php
+++ app/views/layouts/SIG_RD.blade.php
@@ -237,43 +237,21 @@
 
       <!-- Start: Topbar-Dropdown -->
       <div id="topbar-dropmenu" class="alt">
-        <div class="topbar-menu row"><!--
-          <div class="col-xs-4 col-sm-2">
-            <a href="#" class="metro-tile bg-primary light">
-              <span class="glyphicon glyphicon-inbox text-muted"></span>
-              <span class="metro-title">Editar página principal</span>
-            </a>
-          </div>
-          <div class="col-xs-4 col-sm-2">
-            <a href="#" class="metro-tile bg-info light">
-              <span class="glyphicon glyphicon-user text-muted"></span>
-              <span class="metro-title">Administrar Subdirecciones y Jefaturas</span>
-            </a>
-          </div>
-          <div class="col-xs-4 col-sm-2">
-            <a href="#" class="metro-tile bg-success light">
-              <span class="glyphicon glyphicon-headphones text-muted"></span>
-              <span class="metro-title">Administrar contenido SIG</span>
-            </a>
-          </div>
-          <div class="col-xs-4 col-sm-2">
-            <a href="#" class="metro-tile bg-system light">
-              <span class="glyphicon glyphicon-facetime-video text-muted"></span>
-              <span class="metro-title">Editar formatos</span>
-            </a>
-          </div>
-          <div class="col-xs-4 col-sm-2">
-            <a href="#" class="metro-tile bg-alert light">
-              <span class="glyphicon glyphicon-picture text-muted"></span>
-              <span class="metro-title">Administrar control de equipos</span>
-            </a>
-          </div>-->
+        <div class="topbar-menu row">
+
           <div class="col-xs-4 col-sm-2">
             <a href="{{action('SIGController@SIG_RD')}}" class="metro-tile bg-warning light">
               <span class="fa fa-gears text-muted"></span>
               <span class="metro-title">Configuración de SIG</span>
             </a>
           </div>
+          <div class="col-xs-4 col-sm-2">
+            <a href="{{action('SIGController@editarOrganigrama')}}" class="metro-tile bg-warning light">
+              <span class="fa fa-sitemap text-muted"></span>
+              <span class="metro-title">Editar organigramas</span>
+            </a>
+          </div>
+
         </div>
       </div>
       <!-- End: Topbar-Dropdown -->
